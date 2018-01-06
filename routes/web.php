27b0f2910b34f37a88543c0abfd41ea4764ec667
Sjoerdh95 <sjoerdh.sh@gmail.com>
2018-01-06 22:18:02 +0100

    readded movie resource

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Route::group(['middleware'=>'auth'], function(){
 
     Route::get('/chairselect/{id}', "BioscoopZaalController@index");
     Route::get('/chairselectadmin', "BioscoopZaalController@indexAdmin");
+    Route::resource('movie', 'MovieController');
     Route::get("/moviedetails", "MovieController@details");
     Route::post("/moviereturn", "MovieController@check");
     Route::post("/savemovie", "MovieController@store");
