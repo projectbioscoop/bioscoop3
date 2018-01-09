b0330d50435b01a09ec8f198fc15e6c620b0d60f
alex <d228788@edu.rocwb.nl>
2018-01-09 09:48:52 +0100

    page update

--- resources/views/admin/UpdateAgenda.blade.php
+++ resources/views/admin/UpdateAgenda.blade.php
@@ -4,9 +4,25 @@
 
 <h2>Agenda planning</h2>
 
-<form action="">
+<form action="" class="form-control form-group">
+
+    <label for="movie">Movie</label>
+    <select name="" id="">
+        @foreach(){
+            
+        }
+    </select>
+
     <label for="date" id="date">date</label>
     <input type="date" id="date" name="date">
+
+    <label for="startTime">Start time</label>
+    <select name="startTime" id="startTime">
+        <option value="">15:00 - 17:00</option>
+        <option value="">18:00 - 20:00</option>
+        <option value="">20:30 - 22:00</option>
+        <option value="">22:30 - 24:00</option>
+    </select>
 </form>
 
 @endsection
